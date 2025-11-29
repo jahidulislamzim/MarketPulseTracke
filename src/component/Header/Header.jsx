f1f9506ab49a7216a5d752b13f23356307ba3d62
jahidulislamzim <jahidulislamzim845@gmail.com>
2025-11-29 17:55:52 +0600

    add role based navbar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,16 +1,71 @@
 import React from "react";
-import './Header.css'
 import { Link } from "react-router-dom";
+import "./Header.css";
+
 
 const Header = () => {
+  const role = "buyer"; 
+  const auth = false;
+
+  const handleLogout = () => {
+    console.log("Logged out");
+
+  };
+
+  const renderLinks = () => {
+    switch (role) {
+      case "buyer":
+        return (
+          <>
+            <Link to="/buyer/profile">Profile</Link>
+            <Link to="/buyer/report">Reports</Link>
+            <Link to="/buyer/my-list">My List</Link>
+          </>
+        );
+
+      case "seller":
+        return (
+          <>
+            <Link to="/seller/dashboard">Dashboard</Link>
+            <Link to="/seller/products">My Products</Link>
+            <Link to="/seller/orders">Orders</Link>
+          </>
+        );
+
+      case "admin":
+        return (
+          <>
+            <Link to="/admin/dashboard">Dashboard</Link>
+            <Link to="/admin/users">Users</Link>
+            <Link to="/admin/reports">Reports</Link>
+          </>
+        );
+
+      default:
+        return (
+          <>
+            <Link to="/login">Login</Link>
+          </>
+        );
+    }
+  };
+
   return (
     <div className="navbar">
-      <h2></h2>
-      <div className="nav-links">
-        <Link to="/buyer/Profile">Profile</Link>
-        <Link to="/buyer/report">Repoprts 2</Link>
-        <Link to="/buyer/report">Reports</Link>
-        <Link to="/buyer/my-list">My List</Link>
+      <h2>MPT</h2>
+
+      <div className="right-nav">
+        <div className="nav-links">{renderLinks()}</div>
+
+        <div className="auth-btn">
+          {auth ? (
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
+            <Link to="/login">
+              <button>Login</button>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
